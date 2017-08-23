58a739e2194a47ca38aa430aa2cd050796b46a87
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-23 08:50:53 +0100

    Changed URIs and add components

--- src/index.js
+++ src/index.js
@@ -4,22 +4,27 @@ import './css/reset.css'
 import './css/index.css'
 import './css/home.css'
 import './css/login.css'
-import Home from './Home'
+import App from './App'
+import WatchPage from './components/WatchPage'
+import Home from './components/Home'
 import Login from './components/Login'
 import Logout from './components/Logout'
 
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 function verifySection(nextState, replace) {
   if (!localStorage.getItem('auth-token'))
-    replace('/?msg=You must have been logged to access this content!')
+    replace('/login?msg=You must have been logged to access this content!')
 }
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/home" component={Home} onEnter={verifySection} />
-    <Route path="/" component={Login} />
+    <Route path="/login" component={Login} />
     <Route path="/logout" component={Logout} />
+    <Route path="/" component={App} onEnter={verifySection}>
+      <IndexRoute component={Home} onEnter={verifySection} />
+      <Route path="/watch(/:id)" component={WatchPage} onEnter={verifySection} />
+    </Route >
   </Router>,
   document.getElementById('root'),
 )
