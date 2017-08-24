68a0767770b28fc14841fa7657d2a2c2217320f2
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-24 09:59:54 +0100

    Removed duplicated ID and create an style to APP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import Header from './components/header/Header'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div id="root">
-      <Header />
-      {this.props.children}
-    </div>
+      <div id="App">
+        <Header />
+        {this.props.children}
+      </div>
     )
   }
 }
