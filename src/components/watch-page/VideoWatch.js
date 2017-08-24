3af5cb03c290ac670e5133022cb572224479e767
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-24 10:03:08 +0100

    Refactored video watch component to be dynamic

--- src/components/watch-page/VideoWatch.js
+++ src/components/watch-page/VideoWatch.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
 import ReactStars from 'react-stars'
+import {cleanVideoName} from '../../utils/string'
+import {getVideoUrl} from '../../controllers/VideoActions'
 import './VideoWatch.css'
 
-export default class VideoWatch extends Component {
+class Midia extends Component {
+  render() {
+    const url = this.props.video.url ? getVideoUrl(this.props.video.url) : '';
+    console.log(this.props.video)
+    return (
+      <div className="midia">
+        <video className="midia__video" controls autoPlay>
+          <source
+            src={url}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    )
+  }
+}
+
+class Ratting extends Component {
   setRating(videoId, rating) {
     const requestInfo = {
       method: 'POST',
@@ -22,47 +41,48 @@ export default class VideoWatch extends Component {
       .then(success => console.log(success))
       .catch(err => console.log(err))
   }
-
   render() {
     const ratingChanged = newRating => {
       this.setRating(this.props.videoId, newRating)
     }
 
     return (
+      <div className="video-infos__ratings">
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          value={0}
+          half={false}
+          size={24}
+          color2={'#ffd700'}
+        />
+      </div>
+    )
+  }
+}
+
+class Description extends Component {
+  render() {
+    const text = this.props.video.description
+    return (
+      <p className="video-infos__description">
+        {text}
+      </p>
+    )
+  }
+}
+
+export default class VideoWatch extends Component {
+  render() {
+    const video = this.props.video
+
+    return (
       <div id="VideoWatch">
-        <div className="midia">
-          <video className="midia__video" controls>
-            <source
-              src="https://video-ams3-1.xx.fbcdn.net/v/t43.1792-2/20958471_114096075987204_8430579202100559872_n.mp4?efg=eyJybHIiOjE3OTUsInJsYSI6MTAyNCwidmVuY29kZV90YWciOiJzdmVfaGQifQu00253Du00253D&rl=1795&vabr=1197&oh=7b548158a43c65ae6910884abd7be3fe&oe=599DD8BD"
-              type="video/mp4"
-            />
-          </video>
-        </div>
+        <Midia video={video} />
         <div className="video-infos">
-          <p className="video-infos__title">Meu Video</p>
-          <div className="video-infos__ratings">
-            <ReactStars
-              count={5}
-              onChange={ratingChanged}
-              value={5}
-              half={false}
-              size={24}
-              color2={'#ffd700'}
-            />
-          </div>
-          <p className="video-infos__description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quas
-            perspiciatis itaque ipsa quis? Animi quidem, velit quod dolorem
-            recusandae, nam architecto numquam officiis voluptates sapiente a!
-            Amet quis ex dignissimos soluta similique recusandae vero ullam
-            veniam molestiae nihil in ipsa impedit fuga quisquam eum a, mollitia
-            nobis eius iste eveniet vel repellat, totam voluptatum. Pariatur
-            possimus ex minima itaque corrupti, vero ut quaerat nobis nisi natus
-            dicta, incidunt beatae, dolorum perferendis? Voluptatum excepturi
-            ullam quo asperiores, placeat ex reiciendis ea eum? Nihil, maxime.
-            Quibusdam, aut delectus autem voluptatibus maxime id nemo, est ipsa,
-            optio iusto quod rerum modi ipsum.
-          </p>
+          <p className="video-infos__title">{cleanVideoName(video.name)}</p>
+          <Ratting video={video} />
+          <Description video={video} />
         </div>
       </div>
     )
