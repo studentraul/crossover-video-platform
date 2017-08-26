9999e4f33bfdd547bf6d79db60cf1bd8d091923a
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-26 11:29:10 +0100

    Created tests to <ErrorSpan/> component

--- src/components/common/ErrorSpan.js
+++ src/components/common/ErrorSpan.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './ErrorSpan.css'
 
-export default class ErrorMessage extends Component {
+export default class ErrorSpan extends Component {
   render() {
     const { message } = this.props
     return (
-        <span className="error-login">
-          {message}
-        </span>
-    );
+      <span className="error-login">
+        {message}
+      </span>
+    )
   }
 }
 
-ErrorMessage.propTypes = {
-    message: PropTypes.string.isRequired,
-  }
+ErrorSpan.propTypes = {
+  message: PropTypes.string.isRequired,
+}
+
+ErrorSpan.defaultProps = {
+  message: ''
+}
