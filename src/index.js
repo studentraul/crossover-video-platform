d841561b0c62c5baf6b31758f9017baaeb61bc21
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-26 11:03:17 +0100

    Re-sort imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './css/reset.css'
-import './Index.css'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
 import App from './App'
 import WatchVideo from './components/watch-video/WatchVideo'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
 import Logout from './components/logout/Logout'
 
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import './css/reset.css'
+import './Index.css'
 
 function verifySection(nextState, replace) {
   if (!localStorage.getItem('auth-token'))
